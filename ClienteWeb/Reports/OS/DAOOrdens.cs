28f31cb5c80be5845e35a29b30d585e5b116190c
Márcio das Chagas Ramos <marciochagasramos@gmail.com>
2017-11-30 01:07:12 -0200

    Relatório criado
    Impressão de OS está funcionando.

---
+++ ClienteWeb/Reports/OS/DAOOrdens.cs
@@ -0,0 +1,16 @@
+﻿using Modelo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ClienteWeb.Reports.OS
+{
+    public class DAOOrdens
+    {
+        public List<OrdemItemModelReport> GetItens()
+        {
+            return null;
+        }
+    }
+}
---
+++ ClienteWeb/Reports/OS/OrdemItemModelReport.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ClienteWeb.Reports.OS
+{
+    public class OrdemItemModelReport
+    {
+        public int OrdemItemId { get; set; }
+        public decimal Valor { get; set; }
+        public int Quantidade { get; set; }
+        
+        public int ServicoId { get; set; }
+        public string Descricao { get; set; }
+        public decimal ValorUnitario { get; set; }
+    }
+}
