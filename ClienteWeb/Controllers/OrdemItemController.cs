87bb33d51d710f235ec621fb0e46506bf5cfa055
Elcio Júnior <elcio.junior@workshow.com.br>
2017-12-06 08:56:59 -0200

    Alguns comentarios

--- ClienteWeb/Controllers/OrdemItemController.cs
+++ ClienteWeb/Controllers/OrdemItemController.cs
@@ -14,6 +14,7 @@ namespace ClienteWeb.Controllers
     {
         private Contexto db = new Contexto();
 
+        //lista itens da Ordem
         public ActionResult ListarItens(int id)
         {
             var lista = db.OrdemItens
@@ -25,20 +26,22 @@ namespace ClienteWeb.Controllers
 
             ViewBag.OrdemId = id;
             ViewBag.ServicoId = new SelectList(db.Servicos, "Id", "Descricao");
-
+            //Em Cada iten eu calculo seu valor
             foreach (var item in lista)
             {
                 item.Total = item.Valor * (item.Quantidade);
             }
 
+            //se a lista tiver mais que 0 itens
             if (lista.Count() > 0)
-                ViewBag.TotalItens = lista.Sum(n => n.Valor * n.Quantidade);
+                ViewBag.TotalItens = lista.Sum(n => n.Valor * n.Quantidade);//então calculo total de todos o itens
             else
                 ViewBag.TotalItens = 0;
 
             return PartialView(lista);
         }
         
+        //salvar itens na ordem
         public ActionResult SalvarItens(
             int quantidade, 
             int servicoId, 
@@ -61,6 +64,7 @@ namespace ClienteWeb.Controllers
             return Json(new { Resultado = item.Id }, JsonRequestBehavior.AllowGet);
         }
 
+        //excluir item da ordem
         [HttpPost]
         public ActionResult Excluir(int id)
         {
